4f69be9a5b120eab9027e97677f6a8d2aff67c5b
Milene Taborda <milenetaborda1@outlook.com>
2021-04-11 18:38:16 -0300

    feat: created header component

---
+++ src/components/Header/index.tsx
@@ -0,0 +1,16 @@
+import logo from '../../assets/logo.png'
+import { Container } from './styles'
+
+export function Header() {
+  return(
+    <Container>
+      <div>
+        <img src={logo} alt="Provi"/>
+
+        <button type="button">
+          Nova transação
+        </button>
+      </div>
+    </Container>
+  )
+}
---
+++ src/components/Header/styles.ts
@@ -0,0 +1,34 @@
+import styled from 'styled-components';
+
+export const Container = styled.div`
+  background: #bbdefb;
+  padding: 2rem;
+
+  div {
+    max-width: 1120px;
+    margin: 0 auto;
+    padding: 0 1rem 10rem;
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+
+    img {
+      width: 100px;
+    }
+
+    button {
+      font-size: 1rem;
+      color: #fff;
+      background: #187bcd;
+      border: 0;
+      padding: 0 2rem;
+      border-radius: 0.25rem;
+      height: 3rem;
+      transition: filter 0.2s;
+
+      :hover {
+        filter: brightness(0.9);
+      }
+    }
+  }
+`;
