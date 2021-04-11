31faf62374d1454c235e49146def34ec56ec0b44
Milene Taborda <milenetaborda1@outlook.com>
2021-04-11 18:04:41 -0300

    chore: initial commit with folder structure made

---
+++ src/styles/global.ts
@@ -0,0 +1,40 @@
+import {  createGlobalStyle } from 'styled-components';
+
+export const GlobalStyle = createGlobalStyle`
+  :root {
+    --background: #f8f2f5;
+
+    --text-title: #363f5f;
+    --text-body: #969c85;
+  }
+
+  * {
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+  }
+
+  html {
+    @media(max-width: 1080px) {
+      font-size: 93.75%;
+    }
+
+    @media(max-width: 720px) {
+      font-size: 87.5%;
+    } 
+  }
+
+  body {
+    background: var(--background);
+    -webkit-font-smoothing: antialiesed;
+  }
+
+  border, input, button {
+    font-family: 'Poppins', sans-serif;
+  }
+  
+  button {
+    cursor: pointer; 
+  }
+
+`;
