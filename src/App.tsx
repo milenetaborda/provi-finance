d7ea10870cea6e5e156269f7d2d88ca916e71bf7
Milene Taborda <milenetaborda1@outlook.com>
2021-04-12 00:46:53 -0300

    feat: initial mirage setup

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,27 @@ import { Header } from "./components/Header";
 
 import { GlobalStyle } from "./styles/global";
 
+import { createServer } from 'miragejs';
+
+createServer({
+  routes() {
+    this.namespace = 'api';
+
+    this.get('/transactions', () => {
+      return [
+        {
+          id: 1,
+          title: 'Transaction 1',
+          amount: 600,
+          type: 'deposit',
+          category: 'Casa',
+          createdAt: new Date()
+        }
+      ]
+    })
+  }
+});
+
 function App() {
   return (
     <>
