89fb89dc535b93d33ed52ff2d0b472278264c3b0
Milene Taborda <milenetaborda1@outlook.com>
2021-04-11 22:51:07 -0300

    feat: created summary component

---
+++ src/components/Summary/index.tsx
@@ -0,0 +1,33 @@
+import incomeIcon from '../../assets/income.svg';
+import outcomeIcon from '../../assets/outcome.svg';
+import totalIcon from '../../assets/total.svg';
+
+import { Container } from "./styles";
+
+export function Summary() {
+  return (
+    <Container>
+      <div>
+        <header>
+          <p>Entradas</p>
+          <img src={incomeIcon} alt="Entradas"/>
+        </header>
+        <strong>R$2000,00</strong>
+      </div>
+      <div>
+        <header>
+          <p>Saídas</p>
+          <img src={outcomeIcon} alt="Saídas"/>
+        </header>
+        <strong>- R$200,00</strong>
+      </div>
+      <div>
+        <header>
+          <p>Total</p>
+          <img src={totalIcon} alt="Total"/>
+        </header>
+        <strong>R$2000,00</strong>
+      </div>
+    </Container>
+  )
+}
